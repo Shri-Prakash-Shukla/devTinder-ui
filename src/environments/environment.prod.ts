671b3c9ef0b772967a043d024ad1aaf18f8fbce3
Shri Prakash Shukla <shriprakashshukla532@gmail.com>
2026-07-05 13:47:33 +0530

    Missed env folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  googleClientId: '409096554769-i8hp31tin833dlt7be36qvfj4pms9bd0.apps.googleusercontent.com',
+  googleRedirectUri: 'http://devtinders.duckdns.org/api/auth/callback'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  googleClientId: '409096554769-i8hp31tin833dlt7be36qvfj4pms9bd0.apps.googleusercontent.com',
+  googleRedirectUri: 'http://localhost:3000/auth/callback'
+};
